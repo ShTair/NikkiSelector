2e5acfc409cef40a59eea921c654043b6bf43394
ShTair <a@vsh.jp>
2017-03-23 01:10:08 +0900

    モデルの準備

---
+++ NikkiSelector/Models/Item.cs
@@ -0,0 +1,36 @@
+﻿namespace NikkiSelector.Models
+{
+    class Item
+    {
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string Kind { get; set; }
+
+        public string Memo1 { get; set; }
+
+        public string Name { get; set; }
+
+        public string Rarity { get; set; }
+
+        public string P11 { get; set; }
+        public string P12 { get; set; }
+
+        public string P21 { get; set; }
+        public string P22 { get; set; }
+
+        public string P31 { get; set; }
+        public string P32 { get; set; }
+
+        public string P41 { get; set; }
+        public string P42 { get; set; }
+
+        public string P51 { get; set; }
+        public string P52 { get; set; }
+
+        public string Tags { get; set; }
+
+        public string Color { get; set; }
+    }
+}
