d58b55846c374a28fcf9103856b88943c9406020
ShTair <git@vsh.jp>
2017-06-02 02:14:22 +0900

    タグでの調整

--- NikkiSelector/Program.cs
+++ NikkiSelector/Program.cs
@@ -12,7 +12,11 @@ namespace NikkiSelector
         {
             var pm = new Dictionary<string, int> { { "", 0 }, { "X", 0 }, { "D", 2 }, { "C", 3 }, { "B", 4 }, { "A", 5 }, { "S", 7 }, { "SS", 9 }, { "SSS", 11 }, { "SSSS", 13 } };
 
-            var vs = args[1].Split(',').Select(t => double.Parse(t)).ToList();
+            var vs = args[1].Split(',').Select(t => int.Parse(t)).ToList();
+
+            var tss = "";
+            if (args.Length >= 3) tss = args[2];
+            var ts = tss.Split(',').Where(t => !string.IsNullOrWhiteSpace(t)).ToList();
 
             List<Item> items;
             using (var reader = File.OpenText(args[0]))
@@ -24,6 +28,7 @@ namespace NikkiSelector
             }
 
             var gs = items.GroupBy(t => t.Kind);
+            Directory.Delete("res", true);
             Directory.CreateDirectory("res");
 
             int XNum(string v)
@@ -85,9 +90,20 @@ namespace NikkiSelector
             foreach (var g in gs)
             {
                 var name = g.Key;
-                var path = $"res\\i_{XNum(name)}_{name}.txt";
+                var od = XNum(name);
 
-                File.WriteAllLines(path, g.Select(t => (pm[t.P11] * vs[0] + pm[t.P12] * vs[1] + pm[t.P21] * vs[2] + pm[t.P22] * vs[3] + pm[t.P31] * vs[4] + pm[t.P32] * vs[5] + pm[t.P41] * vs[6] + pm[t.P42] * vs[7] + pm[t.P51] * vs[8] + pm[t.P52] * vs[9], t)).OrderByDescending(t => t.Item1).Select(t => $"{t.Item1:000.00},{t.Item2.Id:00000},{t.Item2.Name}"));
+                IEnumerable<(int, Item)> r = g.Select(t => (pm[t.P11] * vs[0] + pm[t.P12] * vs[1] + pm[t.P21] * vs[2] + pm[t.P22] * vs[3] + pm[t.P31] * vs[4] + pm[t.P32] * vs[5] + pm[t.P41] * vs[6] + pm[t.P42] * vs[7] + pm[t.P51] * vs[8] + pm[t.P52] * vs[9], t)).OrderByDescending(t => t.Item1);
+
+                var pi = $"res\\i_{od}_1_{name}.txt";
+                File.WriteAllLines(pi, r.Select(t => $"{t.Item1:0000},{t.Item2.Id:00000},{t.Item2.Name}"));
+
+                if (ts.Count != 0)
+                {
+                    r = r.Where(t => t.Item2.Tags.Split(' ').Any(t2 => ts.Contains(t2)));
+
+                    var pt = $"res\\i_{od}_0_{name}.txt";
+                    File.WriteAllLines(pt, r.Select(t => $"{t.Item1:0000},{t.Item2.Id:00000},{t.Item2.Name}"));
+                }
             }
         }
     }
