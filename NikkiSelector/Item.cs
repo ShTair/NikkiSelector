1d74c8dfc649b9a998376e9e3f0b65c89d232169
ShTair <git@vsh.jp>
2017-06-01 23:30:27 +0900

    CSV読み込み

---
+++ NikkiSelector/Item.cs
@@ -0,0 +1,71 @@
+﻿using CsvHelper.Configuration;
+
+namespace NikkiSelector
+{
+    class Item
+    {
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string Kind { get; set; }
+
+        public string Memo1 { get; set; }
+
+        public string Name { get; set; }
+
+        public string Rarity { get; set; }
+
+        public string P11 { get; set; }
+        public string P12 { get; set; }
+
+        public string P21 { get; set; }
+        public string P22 { get; set; }
+
+        public string P31 { get; set; }
+        public string P32 { get; set; }
+
+        public string P41 { get; set; }
+        public string P42 { get; set; }
+
+        public string P51 { get; set; }
+        public string P52 { get; set; }
+
+        public string Tags { get; set; }
+
+        public string Color { get; set; }
+
+        public bool HasName => !string.IsNullOrWhiteSpace(Name);
+
+        public bool HasAllData => !string.IsNullOrWhiteSpace(P11 + P12)
+                && !string.IsNullOrWhiteSpace(P21 + P22)
+                && !string.IsNullOrWhiteSpace(P31 + P32)
+                && !string.IsNullOrWhiteSpace(P41 + P42)
+                && !string.IsNullOrWhiteSpace(P51 + P52);
+
+        public sealed class Map : CsvClassMap<Item>
+        {
+            public Map()
+            {
+                Map(m => m.Id).Index(0);
+                Map(m => m.ItemId).Index(1);
+                Map(m => m.Kind).Index(2);
+                Map(m => m.Memo1).Index(3);
+                Map(m => m.Name).Index(4);
+                Map(m => m.Rarity).Index(5);
+                Map(m => m.P11).Index(6);
+                Map(m => m.P12).Index(7);
+                Map(m => m.P21).Index(8);
+                Map(m => m.P22).Index(9);
+                Map(m => m.P31).Index(10);
+                Map(m => m.P32).Index(11);
+                Map(m => m.P41).Index(12);
+                Map(m => m.P42).Index(13);
+                Map(m => m.P51).Index(14);
+                Map(m => m.P52).Index(15);
+                Map(m => m.Tags).Index(16);
+                Map(m => m.Color).Index(17);
+            }
+        }
+    }
+}
--- NikkiSelector/Program.cs
+++ NikkiSelector/Program.cs
@@ -1,9 +1,7 @@
-﻿using Sgml;
+﻿using CsvHelper;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Xml;
 
 namespace NikkiSelector
 {
@@ -11,97 +9,13 @@ namespace NikkiSelector
     {
         static void Main(string[] args)
         {
-            Load("1_ヘアスタイル", "https://miraclenikki.gamerch.com/%E3%83%98%E3%82%A2%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB");
-            Load("2_ドレス", "https://miraclenikki.gamerch.com/%E3%83%89%E3%83%AC%E3%82%B9");
-            Load("3_コート", "https://miraclenikki.gamerch.com/%E3%82%B3%E3%83%BC%E3%83%88");
-            Load("4_トップス", "https://miraclenikki.gamerch.com/%E3%83%88%E3%83%83%E3%83%97%E3%82%B9");
-            Load("5_ボトムス", "https://miraclenikki.gamerch.com/%E3%83%9C%E3%83%88%E3%83%A0%E3%82%B9");
-            Load("6_靴下", "https://miraclenikki.gamerch.com/%E9%9D%B4%E4%B8%8B");
-            Load("7_シューズ", "https://miraclenikki.gamerch.com/%E3%82%B7%E3%83%A5%E3%83%BC%E3%82%BA");
-            Load("15_メイク", "https://miraclenikki.gamerch.com/%E3%83%A1%E3%82%A4%E3%82%AF");
-
-            Load("8_ヘアアクセサリー", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E9%A0%AD");
-            Load("9_耳飾り", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E8%80%B3");
-            Load("10_首飾り", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E9%A6%96");
-            Load("11_腕飾り", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E8%85%95");
-            Load("12_手持品", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E6%89%8B");
-            Load("13_腰飾り", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E8%85%B0");
-            Load("14_特殊", "https://miraclenikki.gamerch.com/%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B5%E3%83%AA%E3%83%BC%E3%83%BB%E7%89%B9%E6%AE%8A");
-        }
-
-        private static void Load(string name, string uri)
-        {
-            var z = new double[] { 0, 2.0 / 3, 3.0 / 3, 4.0 / 3 };
-
-            var vs = new double[] {
-                z[0]/* 華麗 */,
-                z[0]/* シンプル */,
-
-                z[0]/* エレガント */,
-                z[0]/* アクティブ */,
-
-                z[0]/* 大人 */,
-                z[0]/* キュート */,
-
-                z[0]/* セクシー */,
-                z[0]/* ピュア */,
-
-                z[0]/* ウォーム */,
-                z[0]/* クール */,
-            };
-
-            using (var wc = new WebClient())
+            Dictionary<int, Item> items;
+            using (var reader = File.OpenText(args[0]))
+            using (var csv = new CsvReader(reader))
             {
-                wc.Encoding = Encoding.UTF8;
-                using (var sgml = new SgmlReader { Href = uri })
-                {
-                    var doc = new XmlDocument();
-                    doc.Load(sgml);
-
-                    bool h = true;
-                    using (var stream = new StreamWriter(name + ".csv", false, Encoding.Default))
-                    {
-                        double cv(string v)
-                        {
-                            switch (v.Trim())
-                            {
-                                case "X": return 0;
-                                case "D": return 2;
-                                case "C": return 3;
-                                case "B": return 4;
-                                case "A": return 5;
-                                case "S": return 7;
-                                case "SS": return 9;
-                                case "SSS": return 11;
-                                case "SSSS": return 13;
-                            }
-
-                            return 0;
-                        }
-
-                        foreach (XmlElement elem in doc.GetElementsByTagName("table").Cast<XmlElement>())
-                        {
-                            var tid = elem.GetAttribute("id");
-                            if (!tid.StartsWith("ui_wikidb_table_")) continue;
-
-                            if (h)
-                            {
-                                h = false;
-                                stream.WriteLine("score," + string.Join(",", elem.GetElementsByTagName("th").Cast<XmlElement>().Select(t => t.InnerText)));
-                            }
-
-                            foreach (var item in from trs in elem.GetElementsByTagName("tr").Cast<XmlElement>()
-                                                 let cs = trs.GetElementsByTagName("td").Cast<XmlElement>().ToList()
-                                                 where cs.Count != 0
-                                                 select cs.Select(t => t.InnerText).ToList())
-                            {
-                                var r = item.Skip(4).Take(10).Select(t => cv(t)).ToList();
-                                var sc = Enumerable.Range(0, 10).Select(t => r[t] * vs[t]).Sum();
-                                stream.WriteLine(sc + "," + string.Join(",", item));
-                            }
-                        }
-                    }
-                }
+                csv.Configuration.RegisterClassMap<Item.Map>();
+                csv.Configuration.HasHeaderRecord = false;
+                items = csv.GetRecords<Item>().ToDictionary(t => t.Id, t => t);
             }
         }
     }
